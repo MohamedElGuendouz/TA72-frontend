b001d9531808179176dc858c98311a61965f9b52
MohamedElGuendouz <37230008+MohamedElGuendouz@users.noreply.github.com>
2021-01-02 12:42:02 +0100

    Create Composants and Services #3

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessagerieComponent } from './messagerie/messagerie.component';
+import { MessageDetailComponent } from './message-detail/message-detail.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { CreateMessageComponent } from './create-message/create-message.component';
+import { ForumComponent } from './forum/forum.component';
+import { SubjectDetailComponent } from './subject-detail/subject-detail.component';
+import { SubjectListComponent } from './subject-list/subject-list.component';
+import { CreateSubjectComponent } from './create-subject/create-subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagerieComponent,
+    MessageDetailComponent,
+    MessageListComponent,
+    CreateMessageComponent,
+    ForumComponent,
+    SubjectDetailComponent,
+    SubjectListComponent,
+    CreateSubjectComponent
   ],
   imports: [
     BrowserModule,
